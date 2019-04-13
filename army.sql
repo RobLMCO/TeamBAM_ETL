69506e0e97030f43ae5a419b55b3d8a429c16706
Rob Phillips <robbie.phillips@lmco.com>
2019-04-12 22:44:39 -0400

    MySQL script to query army base location data

---
+++ army.sql
@@ -0,0 +1,89 @@
+CREATE database army_database;
+USE army_database;
+
+/*show table creation*/
+
+select * from base_locations;
+
+/* sum quantities filtered by type */
+
+select sum(Quantity)
+from base_locations
+where Type = "Air RVCT";
+
+select * from base_locations
+where Type = "Air RVCT";
+
+select sum(Quantity)
+from base_locations
+where Type = "Ground RVCT";
+
+select Name, State, Latitude, Longitude, Type, Quantity from base_locations
+where Type = "Ground RVCT";
+
+/* sum quantities filtered by region */
+
+select Name, State, Latitude, Longitude, Type, Quantity from base_locations
+where Region = "CONUS";
+
+select sum(Quantity)
+from base_locations
+where Region = "CONUS";
+
+
+select Name, State, Latitude, Longitude, Type, Quantity from base_locations
+where Region = "OCONUS";
+
+
+
+select sum(Quantity)
+from base_locations
+where Region = "OCONUS"; 
+
+select Name, State, Latitude, Longitude, Type, Quantity
+from base_locations;
+
+select Name, State, Latitude, Longitude, Type, Quantity
+from base_locations
+where Command = "FORSCOM";
+
+select sum(Quantity)
+from base_locations
+where Command = "FORSCOM";
+
+select Name, State, Latitude, Longitude, Type, Quantity
+from base_locations
+where Command = "USAPAC";
+
+select sum(Quantity)
+from base_locations
+where Command = "USPSAC";
+
+select Name, State, Latitude, Longitude, Type, Quantity
+from base_locations
+where Command = "TRADOC";
+
+select sum(Quantity)
+from base_locations
+where Command = "TRADOC";
+
+select Name, State, Latitude, Longitude, Type, Quantity
+from base_locations
+where Command = "Training";
+
+select sum(Quantity)
+from base_locations
+where Command = "Training";
+
+select Name, State, Latitude, Longitude, Type, Quantity
+from base_locations
+where Command = "ARNG";
+
+select sum(Quantity)
+from base_locations
+where Command = "ARNG";
+
+
+
+
+
