d6247d8eb884dc45c10a15c0d5a34b1fbfccb67b
Rob Phillips <robbie.phillips@lmco.com>
2019-04-14 00:46:27 -0400

    Add files via upload

--- army.py
+++ army.py
@@ -11,24 +11,15 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, render_template
+army_file = "base_locations.csv"
+army_df = pd.read_csv(army_file)
 
-engine = create_engine("sqlite:///Resources/army.sqlite?check_same_thread=False")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# We can view all of the classes that automap found
-Base.classes.keys()
-
-# Save references to each table
-#Measurement = Base.classes.measurement
-#Station = Base.classes.station
-
-session = Session(engine)
-
-base_locations_dict = {'name:', 'chicken'}
+engine = create_engine("sqlite:///army_1.sqlite", echo=False)
+engine.table_names()
+try:
+    army_df.to_sql(name='base_locations', con=engine, index=True)
+except:
+    pass
 
 #################################################
 # Flask Setup
@@ -70,107 +61,133 @@ def api():
 @app.route("/api/v1.0/all")
 def all():
     
-    #tableName = 'someTable'
-    #engine = sqlalchemy.create_engine('connectionString')
-    #Session = scoped_session(sessionmaker(bind=engine))
-    #currentSession = Session()
-    #query = 'IF ('
-                      # 'EXISTS ('
-                      # 'SELECT * FROM INFORMATION_SCHEMA.TABLES '
-                      # 'WHERE TABLE_SCHEMA = \'dbo\' '
-                      # 'AND TABLE_NAME = \'%s\')) '
-                      # 'BEGIN '
-                      # 'DELETE FROM [database].[dbo].[%s] '
-                      # 'END' % (tableName,tableName)
-    #session.execute(query)
-    #print(query)
+    
     return render_template('ALL.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 
 @app.route("/api/v1.0/commands/FORSCOM")
 def FORSCOM():
-    #stations_data = session.query(Measurement.station).all()
+    
     return render_template('FORSCOM.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/commands/CENTCOM")
 def CENTCOM():
-    #stations_data = session.query(Measurement.station).all()
+    
     return render_template('CENTCOM.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/commands/ARNG")
 def ARNG():
     
     return render_template('ARNG.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/commands/Training")
 def TRNG():
     
-    #startdate_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-    #    filter(Measurement.date >= start_date).all()
+    
     return render_template('Training.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+   
 
 @app.route("/api/v1.0/commands/USARPAC")
 def USARPAC():
     
-    #startenddates_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-    #    filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    
     return render_template('USARPAC.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/commands/USAREUR")
 def USAREUR():
     
-    #startdate_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-    #    filter(Measurement.date >= start_date).all()
+    
     return render_template('USAREUR.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/commands/SOF")
 def SOF():
     
-    #startenddates_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-    #    filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    
     return render_template('SOCCOM.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/commands/TRADOC")
 def TRADOC():
     
-    #startenddates_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-    #    filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    
     return render_template('TRADOC.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    
 
 @app.route("/api/v1.0/regions/CONUS")
 def CONUS():
+    results = []
+    conus_quantity = 0
+    conus_quantity=engine.execute("SELECT SUM(Quantity) from base_locations where Region='CONUS'")
+
+    for total in conus_quantity:
+        print(total)
     
-    return render_template('CONUS.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    conus=engine.execute("SELECT * from base_locations where Region='CONUS'")
+    for result in conus:
+        results.append(result)
+        print(result)
+    
+    return render_template('CONUS.html', total=total, results=results)
+    
 
 @app.route("/api/v1.0/regions/OCONUS")
 def OCONUS():
+    results = []
+    oconus_quantity = 0
+    oconus_quantity=engine.execute("SELECT SUM(Quantity) from base_locations where Region='OCONUS'")
+
+    for total in oconus_quantity:
+        print(total)
     
-    return render_template('OCONUS.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    oconus=engine.execute("SELECT * from base_locations where Region='OCONUS'")
+    for result in oconus:
+        results.append(result)
+        print(result)
+    
+    return render_template('OCONUS.html', total=total, results=results)
+    
 
 @app.route("/api/v1.0/types/Air")
 def AIR():
-    
-    
-    return render_template('AIR.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    results = []
+    air_rvct_quantity = 0
+    air_rvct_quantity=engine.execute("SELECT SUM(Quantity) from base_locations where type='Air RVCT'")
 
+    for total in air_rvct_quantity:
+        print(total)
+    
+    air_rvct=engine.execute("SELECT * from base_locations where type='Air RVCT'")
+
+    for result in air_rvct:
+        results.append(result)
+        print(result)
+
+
+    return render_template('AIR.html', total=total, results=results)
+    
 @app.route("/api/v1.0/types/Ground")
 def GROUND():
+    results = []
+    ground_rvct_quantity = 0
+    ground_rvct_quantity=engine.execute("SELECT SUM(Quantity) from base_locations where type='Ground RVCT'")
+
+    for total in ground_rvct_quantity:
+        print(total)
     
+    ground_rvct=engine.execute("SELECT * from base_locations where type='Ground RVCT'")
+
+    for result in ground_rvct:
+        results.append(result)
+        print(result)
     
-    return render_template('GROUND.html', results="Results go here!")
-    #return jsonify(base_locations_dict)
+    return render_template('GROUND.html', total=total, results=results)
+    
 
 @app.route("/api/v1.0/report")
 def report():
